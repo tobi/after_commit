8f4758557820a3343eda54d0c17fe1ce253a3eea
Dmitry Galinsky <dima.exe@gmail.com>
2009-07-09 14:11:44 +0400

    Made it threadsafe

--- lib/after_commit.rb
+++ lib/after_commit.rb
@@ -1,34 +1,34 @@
 module AfterCommit
   def self.committed_records
-    @@committed_records ||= []
+    Thread.current[:committed_records] ||= []
   end
 
   def self.committed_records=(committed_records)
-    @@committed_records = committed_records
+    Thread.current[:committed_records] = committed_records
   end
   
   def self.committed_records_on_create
-    @@committed_records_on_create ||= []
+    Thread.current[:committed_records_on_create] ||= []
   end
   
   def self.committed_records_on_create=(committed_records)
-    @@committed_records_on_create = committed_records
+    Thread.current[:committed_records_on_create] = committed_records
   end
   
   def self.committed_records_on_update
-    @@committed_records_on_update ||= []
+    Thread.current[:committed_records_on_update] ||= []
   end
   
   def self.committed_records_on_update=(committed_records)
-    @@committed_records_on_update = committed_records
+    Thread.current[:committed_records_on_update] = committed_records
   end
   
   def self.committed_records_on_destroy
-    @@committed_records_on_destroy ||= []
+    Thread.current[:committed_records_on_destroy] ||= []
   end
   
   def self.committed_records_on_destroy=(committed_records)
-    @@committed_records_on_destroy = committed_records
+    Thread.current[:committed_records_on_destroy] = committed_records
   end
 end
 
