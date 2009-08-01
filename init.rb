4cc108bd3844da2b175cae685817c8ce6f01488c
pivotal <pivotal@camp.flood.pivotallabs.com>
2009-07-31 17:30:01 -0700

    Patch the plugin in test mode to avoid interaction with test transactions.

--- init.rb
+++ init.rb
@@ -1,1 +1,10 @@
 require 'after_commit'
+
+ActiveRecord::Base.class_eval do 
+  include AfterCommit::ActiveRecord
+  include AfterCommit::TestBypass if RAILS_ENV == 'test'
+end
+
+Object.subclasses_of(ActiveRecord::ConnectionAdapters::AbstractAdapter).each do |klass|
+  klass.send(:include, AfterCommit::ConnectionAdapters)
+end
