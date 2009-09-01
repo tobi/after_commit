8401b92a288922d0a5e3924f17ae1fc24d5a71d9
David Yip <yipdw@northwestern.edu>
2009-09-01 16:15:26 -0500

    Moved test setup code to a separate helper file.

--- test/after_commit_test.rb
+++ test/after_commit_test.rb
@@ -1,19 +1,4 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
-require 'test/unit'
-require 'rubygems'
-require 'activerecord'
-require 'after_commit'
-require 'after_commit/active_record'
-require 'after_commit/connection_adapters'
-
-ActiveRecord::Base.establish_connection({"adapter" => "sqlite3", "database" => 'test.sqlite3'})
-begin
-  ActiveRecord::Base.connection.execute("drop table mock_records");
-rescue
-end
-ActiveRecord::Base.connection.execute("create table mock_records(id int)");
-
-require File.dirname(__FILE__) + '/../init.rb'
+require File.dirname(__FILE__) + '/test_helper'
 
 class MockRecord < ActiveRecord::Base
   attr_accessor :after_commit_on_create_called
